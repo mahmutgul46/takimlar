2b50a4e6375a4fb22b9f68b5c223540b5ed659bc
mahmut559 <mahmut559@RPC03>
2024-03-07 14:00:26 +0300

    Proje dosyası ekle.

---
+++ takimlar/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace takimlar
+{
+    public partial class Form1 : Form
+    {
+        string[,] takimlar = new string[4, 3]
+        {
+            {"samet","sadettin","selim"},
+            {"muzaffer","eyüp","özlem" },
+            {"erdem","necati","bedirhan"},
+            {"Mehmet","Burak","Ayşe" }
+        };
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show(takimlar[3,0] + " " + takimlar[3,1] + " " + takimlar[3,2]);
+
+            
+        }
+    }
+}
